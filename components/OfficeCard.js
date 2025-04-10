ebf15ac1804aecbca19721c6c4cd0ff700107a84
Baik Seo Woon <sunnyb@hku.hk>
2025-04-11 08:33:36 +0900

    cc

--- components/OfficeCard.js
+++ components/OfficeCard.js
@@ -12,13 +12,11 @@ app.component('office-card', {
     template:
         /* html */
         `
-        <div class="card" style="cursor: pointer; height: 100%; box-shadow: 0 2px 3px rgba(0,0,0,0.1);">
-            <div class="card-image" style="height: 150px; overflow: hidden;">
-                <figure class="image is-3by2" style="height: 100%;">
-                    <img :src="image" :alt="name" class="office-card-image">
-                </figure>
+        <div class="card office-card" style="cursor: pointer; height: 100%; box-shadow: 0 2px 3px rgba(0,0,0,0.1);">
+            <div class="office-card-image-container">
+                <img :src="image" :alt="name" class="office-card-image">
             </div>
-            <div class="card-content p-3">
+            <div class="office-card-text-container">
                 <div class="content has-text-centered">
                     <h2 class="title is-5">{{ name }}</h2>
                 </div>
